883a586af0266f3afdf8a0f6757b7bb5bd3a351d
Mecu Sorin <mecumihaisorin@gmail.com>
2017-12-07 17:10:19 +0200

    Added a template for puzzles

---
+++ AdventOfCode2017.Tests/PuzzleTemplate.fs
@@ -0,0 +1,40 @@
+namespace AdventOfCode2017
+
+module Puzzle =
+
+module PuzzleTests =
+    open Expecto
+    open Puzzle
+
+    let (==?) expected actual = Expect.equal actual expected "Should be equal"
+
+    let basePuzzle1Test testFunction label sample expected = testFunction label <| fun _ ->
+        expected ==?  sample
+    let basePuzzle2Test testFunction label sample expected = testFunction label <| fun _ ->
+        expected ==?  sample
+
+    [<Tests>]
+    let samples =
+        testList
+            "Puzzle "
+            [   testList
+                    "Samples 1"
+                    [   basePuzzle1Test testCase "Sample "
+                        basePuzzle1Test testCase "Sample "
+                        basePuzzle1Test testCase "Sample " ]
+                // testList
+                //     "Samples 2"
+                //     [   basePuzzle2Test testCase "Sample "
+                //         basePuzzle2Test testCase "Sample "
+                //         basePuzzle2Test testCase "Sample " ]
+            ]
+
+
+    let puzzleSample = 
+    [<Tests>]
+    let puzzle1 =
+        testList 
+            "Puzzle "
+            [   basePuzzle1Test testCase "1" puzzleSample
+                basePuzzle2Test testCase "2" puzzleSample
+            ]
